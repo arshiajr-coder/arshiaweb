44fd158dde165c951a45abbf2859867ee94284c1
arshiajr-coder <75935928+arshiajr-coder@users.noreply.github.com>
2023-08-08 10:03:15 +0330

    Update miniprojects.js

--- js/miniprojects.js
+++ js/miniprojects.js
@@ -135,11 +135,23 @@ resetFilter.addEventListener("click", ()=>{
 })
 
 
-const downloadImg = ()=>{
-  const canvas = document.createElement("canvas")
-  const ctx = canvas.getContext("2d")
-  canvas.width = previewImg.naturalWidth
-  canvas.height = previewImg.naturalHeight
+const downloadImg = () => {
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+  canvas.width = previewImg.naturalWidth;
+  canvas.height = previewImg.naturalHeight;
+  ctx.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`;
+  ctx.translate(canvas.width/2, canvas.height/2);
+  if(rotate !== 0){
+     ctx.rotate(rotate * Math.PI / 180);  
+  }
+  ctx.scale(flipX, flipY);
+  ctx.drawImage(previewImg, -canvas.width/2, -canvas.height/2 , canvas.width, canvas.height);
+  
+  const link = document.createElement("a");
+  link.download = "image.jpg";
+  link.href = canvas.toDataURL();
+  link.click();
 
 }
 
