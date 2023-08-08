a6f49ef683a5296cf7061c5d2683c8126306eeeb
arshiajr-coder <75935928+arshiajr-coder@users.noreply.github.com>
2023-08-09 02:27:28 +0330

    update

--- js/miniprojects.js
+++ js/miniprojects.js
@@ -15,6 +15,7 @@ window.addEventListener("DOMContentLoaded", function () {
 });
 
 //////// Picture Editor Code /////
+////////////////////////////////!
 const $ = document;
 const editorBox = $.querySelector(".picture-editor-container");
 const fileInput = $.querySelector(".file-input");
@@ -25,7 +26,7 @@ const filterName = $.querySelector(".filter-name");
 const sliderPrecent = $.querySelector(".slider .value");
 const rotateOptions = $.querySelectorAll(".rotate button");
 const resetFilter = $.querySelector(".reset-filter");
-const saveImgBtn = $.querySelector(".save-img")
+const saveImgBtn = $.querySelector(".save-img");
 
 const loadImg = () => {
   const file = fileInput.files[0];
@@ -56,34 +57,39 @@ const filterOptions = document.querySelectorAll(".filter button");
 let brightness = 100,
   saturation = 100,
   inversion = 0,
-  grayscale = 0,
+  blurW = 0,
   rotate = 0,
   flipY = 1,
   flipX = 1;
 
-const optionsArray = ["brightness", "saturation", "inversion", "grayscale"];
+const optionsArray = ["brightness", "saturation", "inversion", "blur"];
 
 filterOptions.forEach((item) => {
   item.addEventListener("click", () => {
     document.querySelector(".option-active").classList.remove("option-active");
     item.classList.add("option-active");
     filterName.textContent = item.textContent;
-
+    
     if (optionsArray.indexOf(item.id) !== -1) {
-      if (item.id === "grayscale" || item.id === "inversion") {
+      if (item.id === "inversion") {
         filterSlider.max = "100";
+      }
+      if (item.id === "blur") {
+        filterSlider.max = "50";
+        sliderPrecent.textContent = `${blurW}px`;
+        filterSlider.value = `${blurW}`;
       } else {
         filterSlider.max = "200";
+        sliderPrecent.textContent = eval(item.id) + "%";
+        filterSlider.value = eval(item.id);
       }
-      filterSlider.value = eval(item.id);
-      sliderPrecent.textContent = eval(item.id) + "%";
     }
   });
 });
 
 const updateSlider = () => {
   sliderPrecent.textContent = filterSlider.value + "%";
-  
+
   switch (document.querySelector(".option-active").id) {
     case "brightness":
       brightness = filterSlider.value;
@@ -94,11 +100,12 @@ const updateSlider = () => {
     case "inversion":
       inversion = filterSlider.value;
       break;
-    case "grayscale":
-      grayscale = filterSlider.value;
+    case "blur":
+      blurW = filterSlider.value;
+      sliderPrecent.textContent = filterSlider.value + "px";
       break;
   }
-  previewImg.style.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`;
+  previewImg.style.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) blur(${blurW}px)`;
 };
 
 rotateOptions.forEach((item) => {
@@ -121,39 +128,46 @@ rotateOptions.forEach((item) => {
   });
 });
 
-resetFilter.addEventListener("click", ()=>{
-  brightness = 100
-  saturation = 100
-  inversion = 0
-  grayscale = 0
-  rotate = 0
-  flipY = 1
-  flipX = 1
-  filterOptions[0].click()
-  previewImg.style.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`;
+resetFilter.addEventListener("click", () => {
+  brightness = 100;
+  saturation = 100;
+  inversion = 0;
+  blurW = 0;
+  rotate = 0;
+  flipY = 1;
+  flipX = 1;
+  filterOptions[0].click();
+  previewImg.style.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) blur(${blurW}px)`;
   previewImg.style.transform = `rotate(${rotate}deg) scale(${flipX}, ${flipY})`;
-})
-
+});
 
 const downloadImg = () => {
   const canvas = document.createElement("canvas");
   const ctx = canvas.getContext("2d");
   canvas.width = previewImg.naturalWidth;
   canvas.height = previewImg.naturalHeight;
-  ctx.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`;
-  ctx.translate(canvas.width/2, canvas.height/2);
-  if(rotate !== 0){
-     ctx.rotate(rotate * Math.PI / 180);  
+  ctx.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) blur(${blurW}px)`;
+  ctx.translate(canvas.width / 2, canvas.height / 2);
+  if (rotate !== 0) {
+    ctx.rotate((rotate * Math.PI) / 180);
   }
   ctx.scale(flipX, flipY);
-  ctx.drawImage(previewImg, -canvas.width/2, -canvas.height/2 , canvas.width, canvas.height);
-  
-  const link = document.createElement("a");
-  link.download = "image.jpg";
+  ctx.drawImage(
+    previewImg,
+    -canvas.width / 2,
+    -canvas.height / 2,
+    canvas.width,
+    canvas.height
+  );
+
+  const link = $.createElement("a");
+  link.download = "image.png";
   link.href = canvas.toDataURL();
   link.click();
+};
 
-}
-
-saveImgBtn.addEventListener("click", downloadImg)
+saveImgBtn.addEventListener("click", downloadImg);
 filterSlider.addEventListener("input", updateSlider);
+
+// Picture Editor Code /////
+////////////////////////////////!
